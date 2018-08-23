840abd97614e0efe9a95fee726e49e1e682f4fbe
G.Ambatte <sean@robertson-family.nz>
2018-08-23 19:33:43 +1200

    Added initialization code for unused joystick functions Added connection diagram

--- OneButtonJoystick.ino
+++ OneButtonJoystick.ino
@@ -27,6 +27,26 @@ void setup() {
     lastButtonState[index] = 0;
   }
 
+  // Set Joystick Range Values
+  Joystick.setXAxisRange(-127, 127);
+  Joystick.setYAxisRange(-127, 127);
+  Joystick.setZAxisRange(-127, 127);
+  Joystick.setRxAxisRange(0, 360);
+  Joystick.setRyAxisRange(360, 0);
+  Joystick.setRzAxisRange(0, 720);
+  Joystick.setThrottleRange(0, 255);
+  Joystick.setRudderRange(255, 0);
+
+  // Set Joystick values
+  Joystick.setXAxis(0);
+  Joystick.setYAxis(0);
+  Joystick.setZAxis(0);
+  Joystick.setRxAxis(180);
+  Joystick.setRyAxis(180);
+  Joystick.setRzAxis(360);
+  Joystick.setThrottle(128);
+  Joystick.setRudder(128);
+
   // Initialize Joystick Library
   Joystick.begin();
 }
